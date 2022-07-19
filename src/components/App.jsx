ba6951e692ac1afc537da6632fb03833a74a4087
Serhii Kushneruk <svkushneruk@ukr.net>
2022-07-19 23:17:45 +0300

    add Profile Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import user from 'user.json';
+import data from 'data.json';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
