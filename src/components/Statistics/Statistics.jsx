f267ee44b7df9ff2f6e2b756e12395ce43aede46
Serhii Kushneruk <svkushneruk@ukr.net>
2022-07-23 22:35:11 +0300

    fix: render title in Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,8 +5,7 @@ export const Statistics = ({ title, stats }) => {
   return (
     <div>
       <section className={css.statistics}>
-        <h2 className={css.title}>{title}</h2>
-
+        {title && <h2 className={css.title}>{title}</h2>}
         <StatisticList items={stats} />
       </section>
     </div>
